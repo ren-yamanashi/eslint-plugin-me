5306321c494d7f2e9e4c1495cc8c3f56a6a3309b
ren-yamanashi <pinokionigg@icloud.com>
2025-06-06 21:52:44 +0900

    chore: update

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    root: './src',
-    include: ['./__tests__/**/*.test.ts'],
+    include: ['./src/__tests__/**/*.test.ts'],
   },
 });
