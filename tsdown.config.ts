e79e793ce4df58e615446eaa8fc73b7010050eb9
ren-yamanashi <pinokionigg@icloud.com>
2025-06-06 23:05:50 +0900

    chore: build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'tsdown/config';
 
-export default [
-  defineConfig({
-    entry: 'src/index.ts',
-    outDir: 'dist/cjs',
-    clean: true,
-    format: 'cjs',
-    dts: true,
-    outputOptions: { exports: 'named' },
-  }),
-  defineConfig({
-    entry: 'src/index.ts',
-    outDir: 'dist/esm',
-    clean: true,
-    format: 'esm',
-    dts: true,
-    outputOptions: { exports: 'named' },
-  }),
-];
+export default defineConfig({
+  entry: 'src/index.ts',
+  outDir: 'dist',
+  clean: true,
+  format: ['esm', 'cjs'],
+  dts: true,
+  outputOptions: { exports: 'named' },
+});
